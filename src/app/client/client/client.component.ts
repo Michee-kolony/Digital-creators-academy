959de9caeb3dfc8f37837720ab10237834d0d799
Michée Kolony <micheekolony71@gmail.com>
2026-03-29 12:51:01 +0100

    ajout du button whatshapp

--- src/app/client/client/client.component.ts
+++ src/app/client/client/client.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-client',
   templateUrl: './client.component.html',
   styleUrl: './client.component.css'
 })
-export class ClientComponent {
+export class ClientComponent implements OnInit {
+  isPopupOpen = false;
+  showScrollButton = false;
 
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    // Redirection automatique en haut de page
+    window.scrollTo(0, 0);
+    
+    // Optionnel : Écouter les changements de route pour rester en haut
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        window.scrollTo(0, 0);
+      }
+    });
+  }
+
+  togglePopup() {
+    this.isPopupOpen = !this.isPopupOpen;
+  }
+
+  closePopup() {
+    this.isPopupOpen = false;
+  }
+
+  scrollToTop() {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  }
+
+  // Afficher/masquer le bouton en fonction du défilement
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    this.showScrollButton = window.pageYOffset > 300;
+  }
 }
